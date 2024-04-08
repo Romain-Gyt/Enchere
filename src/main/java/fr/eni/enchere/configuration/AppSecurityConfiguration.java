716059bd8595654b2e27b71ff7e76a152ba927af
Frame <guyenot.romain@protonmail.com>
2024-04-08 17:00:20 +0200

    Configuration de security-1

--- src/main/java/fr/eni/enchere/configuration/AppSecurityConfiguration.java
+++ src/main/java/fr/eni/enchere/configuration/AppSecurityConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -33,5 +37,16 @@ public class AppSecurityConfiguration {
                         .permitAll()
         );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        String sqlUsername = "SELECT email, password, 1 FROM MEMBRE WHERE email = ?;";
+        String sqlAuthorities = "SELECT email, ROLE FROM MEMBRE INNER JOIN ROLES ON admin = IS_ADMIN WHERE email = ?";
+        jdbcUserDetailsManager.setUsersByUsernameQuery(sqlUsername);
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(sqlAuthorities);
+
+        return jdbcUserDetailsManager;
     }
 }
