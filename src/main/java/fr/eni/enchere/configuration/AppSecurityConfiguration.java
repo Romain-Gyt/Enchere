e8ab67c13c5e003b0e41533c98be01ebcc40e488
Frame <guyenot.romain@protonmail.com>
2024-04-09 12:17:11 +0200

    Configuration de security-2 Mise en place de la methode "se souvenir de moi"

--- src/main/java/fr/eni/enchere/configuration/AppSecurityConfiguration.java
+++ src/main/java/fr/eni/enchere/configuration/AppSecurityConfiguration.java
@@ -1,21 +1,33 @@
 package fr.eni.enchere.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class AppSecurityConfiguration {
+public class AppSecurityConfiguration  {
+
+
+    @Autowired
+    private  DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -27,7 +39,15 @@ public class AppSecurityConfiguration {
         http.formLogin(form -> {
             form.loginPage("/login").permitAll();
             form.defaultSuccessUrl("/");
+            form.failureUrl("/login?error=true");
         });
+
+        http
+                .rememberMe()
+                .rememberMeParameter("remember_me")
+                .tokenValiditySeconds(86400)
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(60*60*24); // 24 hours
 
         http.logout(logout ->
                 logout
@@ -42,10 +62,22 @@ public class AppSecurityConfiguration {
     }
 
     @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
         String sqlUsername = "SELECT username, password, 1 FROM MEMBRE WHERE username = ?;";
-        String sqlAuthorities = "SELECT username, ROLE FROM MEMBRE INNER JOIN ROLES ON admin = IS_ADMIN WHERE username = ?";
+        String sqlAuthorities = "SELECT username, ROLE FROM MEMBRE INNER JOIN ROLES ON administrator = IS_ADMIN WHERE username = ?";
         jdbcUserDetailsManager.setUsersByUsernameQuery(sqlUsername);
         jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(sqlAuthorities);
 
